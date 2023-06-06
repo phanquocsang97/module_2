a58c27050a12e3b73e238d846e846faf3028e71e
phanquocsang97 <quocsang0310@gmail.com>
2023-06-06 16:41:33 +0700

    done exercise

---
+++ src/ss1_introduce_to_java/exercise/Exercise2.java
@@ -0,0 +1,15 @@
+package ss1_introduce_to_java.exercise;
+
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        double vnd = 230000;
+        double usd;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hãy nhập vào số usd muốn đổi:");
+        usd = sc.nextDouble();
+        double vndQuyDoi = usd * vnd;
+        System.out.println("Số tiền sau khi quy đổi là:" + vndQuyDoi + "VND");
+    }
+}
---
+++ src/ss1_introduce_to_java/practice/Demo.java
@@ -0,0 +1,25 @@
+package ss1_introduce_to_java.practice;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double weight, height, bmi;
+        System.out.println("Your weight(in kilogram): ");
+        weight = scanner.nextDouble();
+        System.out.println("Your height (in meter):");
+        height = scanner.nextDouble();
+        bmi = weight / Math.pow(height, 2);
+        System.out.printf("%-20s%s", "bmi", "Interpretation\n");
+        if (bmi < 18){
+            System.out.println(bmi + " underweight");
+        } else if (bmi < 25.0) {
+            System.out.println(bmi + " normal");
+        } else if (bmi < 30.0) {
+            System.out.printf("%-20.2f%s", bmi, "Overweight");
+        }else {
+            System.out.println(bmi + " obese");
+        }
+    }
+}
