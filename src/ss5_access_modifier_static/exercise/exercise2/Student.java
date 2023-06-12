bca63fdc6ba636b0d9ab9fd1b95285f44462c723
phanquocsang97 <quocsang0310@gmail.com>
2023-06-12 10:22:59 +0700

    done exercise

--- src/ss5_access_modifier_static/exercise/exercise2/Student.java
+++ src/ss5_access_modifier_static/exercise/exercise2/Student.java
@@ -1,12 +1,16 @@
 package ss5_access_modifier_static.exercise.exercise2;
 
 public class Student {
+
     private String name;
     private String classes;
 
     public Student() {
-        name = "Phan Quoc Sang";
-        classes = "C0423G1";
+    }
+
+    public Student(String name, String classes) {
+        this.name = name;
+        this.classes = classes;
     }
 
     public void setName(String name) {
@@ -16,6 +20,13 @@ public class Student {
     public void setClasses(String classes) {
 
         this.classes = classes;
+    }
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", classes='" + classes + '\'' +
+                '}';
     }
 
     public String getName() {
@@ -30,14 +41,18 @@ public class Student {
 
     public static void main(String[] args) {
         Student student = new Student();
+        Student student1 = new Student("sang","co4");
 
         student.setName("Sang");
         student.setClasses("C0423G1");
 
-        String name = student.getName();
-        String classes = student.getClasses();
+//        String name = student.getName();
+//        String classes = student.getClasses();
 
-        System.out.println("Name : " + name);
-        System.out.println("Classes : " + classes);
+//        System.out.println("Name : " + name);
+//        System.out.println("Classes : " + classes);
+        System.out.println(student);
+        System.out.println(student1);
+
     }
 }
